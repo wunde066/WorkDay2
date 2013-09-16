40af89f024200fe7de7825d8fe14bc0143b09956
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:24:23 -0500

    we did it

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -4,5 +4,23 @@ Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
 
+        Dim fullname As String = txtFullName.Text
+
+        Dim fullnametrimmed As String
+
+        fullnametrimmed = fullname.Trim()
+
+        fullname = txtFullName.Text
+
+        Dim idxspace As Integer
+        idxspace = fullnametrimmed.IndexOf(" ")
+
+        Dim firstname As String
+
+        firstname = fullnametrimmed.Substring(0, idxspace)
+
+        txtfirstname.Text = FirstName
+
     End Sub
+
 End Class
